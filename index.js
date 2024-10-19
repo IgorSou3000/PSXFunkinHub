b21f8e4e8a11ede240db71326c2c6080baa1b90d
IgorSou3000 <90409597+IgorSou3000@users.noreply.github.com>
2024-10-19 11:24:06 -0300

    first commit lol

---
+++ index.js
@@ -0,0 +1,198 @@
+const ports = [
+	{
+		name: 'VS Zardy',
+		link: 'https://gamebanana.com/mods/326248',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'VS Djentbot',
+		link: 'https://gamebanana.com/mods/327595',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Friday Night Funkin Neo',
+		link: 'https://gamebanana.com/mods/329221',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Friday Night Drownin',
+		link: 'https://gamebanana.com/mods/333032',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'VS Matt',
+		link: 'https://gamebanana.com/mods/335393',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Tails Gets Trolled',
+		link: 'https://gamebanana.com/mods/336197',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Cyber Sensation',
+		link: 'https://gamebanana.com/mods/345700',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'VS QT',
+		link: 'https://gamebanana.com/mods/349732',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'VS Tails.EXE',
+		link: 'https://gamebanana.com/mods/351008',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Boogie-Man',
+		link: 'https://gamebanana.com/mods/355297',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Among us over Week 1',
+		link: 'https://gamebanana.com/mods/356578',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Confronting Yourself',
+		link: 'https://gamebanana.com/mods/364874',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Skeleton Bros',
+		link: 'https://gamebanana.com/mods/372077',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Pibby Corrupted',
+		link: 'https://gamebanana.com/mods/388045',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'Funktime Bunzo',
+		link: 'https://gamebanana.com/mods/391158',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'VS Walter',
+		link: 'https://gamebanana.com/mods/403045',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'CN Takeover',
+		link: 'https://gamebanana.com/mods/450427',
+		creator: 'Unstop4ble',
+	},
+	{
+		name: 'VS Garcello',
+		link: 'https://gamebanana.com/mods/325140',
+		creator: 'IgorSou3000',
+	},
+	{
+		name: 'Friday Night Funkin D Side',
+		link: 'https://gamebanana.com/mods/338743',
+		creator: 'IgorSou3000',
+	},
+	{
+		name: 'Red Version - VS Red',
+		link: 'https://gamebanana.com/mods/345716',
+		creator: 'IgorSou3000',
+	},
+	{
+		name: 'Chaos Nightmare',
+		link: 'https://gamebanana.com/mods/382595',
+		creator: 'IgorSou3000',
+	},
+	{
+		name: 'GF.hx',
+		link: 'https://gamebanana.com/mods/398460',
+		creator: 'IgorSou3000',
+	},
+	{
+		name: 'VS Sonic Says',
+		link: 'https://gamebanana.com/mods/500545',
+		creator: 'IgorSou3000',
+	},
+	{
+		name: 'Duo Pack',
+		link: 'https://gamebanana.com/mods/317068',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'Around The Fire',
+		link: 'https://gamebanana.com/mods/318502',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'Sonic.EXE 1.5',
+		link: 'https://gamebanana.com/mods/3194168',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'Friday Night Wumpin',
+		link: 'https://gamebanana.com/mods/321100',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'VS Impostor V3',
+		link: 'https://gamebanana.com/mods/323240',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'Friday Night Funkin With Parappa',
+		link: 'https://gamebanana.com/mods/329625',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'VS Mickey Mouse',
+		link: 'https://gamebanana.com/mods/339042',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'VS Tricky',
+		link: 'https://gamebanana.com/mods/385882',
+		creator: 'Lord Scout',
+	},
+	{
+		name: 'VS Tan',
+		link: 'https://gamebanana.com/mods/330204',
+		creator: 'BiliousData',
+	},
+	{
+		name: 'VS Eteled',
+		link: 'https://gamebanana.com/mods/351910',
+		creator: 'BiliousData',
+	},
+];
+const container = document.getElementById('mainContainer');
+
+const portsCounter = document.getElementById('portsCounter');
+const portsCounterHTML = ports.length;
+
+function compareByName(a, b) {
+	if (a.name < b.name)
+		return -1;
+
+	if (a.name > b.name)
+		return 1;
+
+	return 0;
+};
+
+function createPortsHTML(container, ports) {
+	ports.forEach(port => {
+		const portImage = 'img/' + port.name + '.png';
+		const portHTML = `
+		<figure class="port">
+			<img src="${portImage}" alt="${port.name} image" width=300>
+			<figcaption><a href="${port.link}" target="_blank">${port.name}</a></figcaption>`;
+		
+		container.insertAdjacentHTML('beforeend', portHTML);
+	});
+};
+
+ports.sort(compareByName);
+
+createPortsHTML(container, ports);
+portsCounter.insertAdjacentHTML('beforeend', portsCounterHTML);
